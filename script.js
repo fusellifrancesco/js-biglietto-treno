2bdbdbe015d3240db938b1c7abf4c65707bbfa8f
fusellifrancesco <fusellifrancesco05@gmail.com>
2022-11-10 14:19:40 +0100

    Dichiarate variabili prezzo biglietto

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ let eta = prompt("Inserisci la tua età");
 let chilometri = prompt("Inserisci quanti chilometri desideri percorrere");
 const prezzoAlKm = 0.21;
 
+let prezzoBiglietto = chilometri * prezzoAlKm;
+
+let prezzoMinorenni = prezzoBiglietto * 20 / 100;
+let prezzoOver65 = prezzoBiglietto * 40 / 100;
+
+
