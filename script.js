7994f3bd265ca4b00e89c78e4ae90bb6a3eee188
fusellifrancesco <fusellifrancesco05@gmail.com>
2022-11-10 15:54:58 +0100

    modifica titolo pagina e istruzione IF

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
+let nome = prompt("Inserisci il tuo nome");
+let cognome = prompt("Inserisci il tuo cognome");
 let eta = prompt("Inserisci la tua età");
-let chilometri = prompt("Inserisci quanti chilometri desideri percorrere");
+let chilometri = prompt("Inserisci i chilometri che desideri percorrere");
+
 const prezzoAlKm = 0.21;
 
 let prezzoBigliettoIntero = chilometri * prezzoAlKm;
@@ -7,13 +10,53 @@ let prezzoBigliettoIntero = chilometri * prezzoAlKm;
 let prezzoUnder18 = prezzoBigliettoIntero - (prezzoBigliettoIntero * 0.20);
 let prezzoOver65 = prezzoBigliettoIntero - (prezzoBigliettoIntero * 0.40);
 
-let prezzoOver2decimali = prezzoOver65.toFixed(2);
+let prezzoOver2Decimali = prezzoOver65.toFixed(2);
 let prezzoUnder2Decimali = prezzoUnder18.toFixed(2)
+let prezzoBigliettoInteroDecimale = prezzoBigliettoIntero.toFixed(2)
 
 if (eta < 18){
-    console.log(prezzoUnder2Decimali);
+    console.log(
+        `
+    --------- DETTAGLI BIGLIETTO ------------
+        Nome: ${nome}
+        Cognome: ${cognome}
+        Età: ${eta}
+
+    --------- KM DA PERCORRERE ------------
+        KM: ${chilometri}
+
+    ------ PREZZO BIGLIETTO ---------
+        € ${prezzoUnder2Decimali}
+    `
+    );
 } else if (eta >= 65){
-    console.log(prezzoOver2decimali);
+    console.log(
+        `
+    --------- DETTAGLI BIGLIETTO ------------
+        Nome: ${nome}
+        Cognome: ${cognome}
+        Età: ${eta}
+
+    --------- KM DA PERCORRERE ------------
+        KM: ${chilometri}
+
+    ------ PREZZO BIGLIETTO ---------
+        € ${prezzoOver2Decimali}
+    `
+    );
 } else {
-    console.log(prezzoBigliettoIntero)
+    console.log(
+        `
+    --------- DETTAGLI BIGLIETTO ------------
+        Nome: ${nome}
+        Cognome: ${cognome}
+        Età: ${eta}
+
+    --------- KM DA PERCORRERE ------------
+        KM: ${chilometri}
+
+    ------ PREZZO BIGLIETTO ---------
+        € ${prezzoBigliettoInteroDecimale}
+    `
+    );
 }
