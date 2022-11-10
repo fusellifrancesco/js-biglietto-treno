361d5f22541a7d081bb42269770210c8c92fbde2
fusellifrancesco <fusellifrancesco05@gmail.com>
2022-11-10 14:39:27 +0100

    Aggiunto metodo per visualizzare solo 2 decimali

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ const prezzoAlKm = 0.21;
 
 let prezzoBiglietto = chilometri * prezzoAlKm;
 
-let prezzoMinorenni = prezzoBiglietto * 20 / 100;
-let prezzoOver65 = prezzoBiglietto * 40 / 100;
+let prezzoMinorenni = prezzoBiglietto * 0.20;
+let prezzoOver65 = prezzoBiglietto * 0.40;
 
-
+let prezzoOver2decimali = prezzoOver65.toFixed(2);
+let prezzoMinorenni2Decimali = prezzoMinorenni.toFixed(2)
